9aa213a150e9f46a841d1a92865952c101a195c8
Abdiwjilo <abdiwjilo@gmail.com>
2021-09-18 18:35:56 -0500

    Create association From Admin to User

--- src/main/java/edu/miu/Administrator.java
+++ src/main/java/edu/miu/Administrator.java
@@ -1,20 +1,41 @@
 package edu.miu;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Administrator extends Role{
 private List<Advertise> advertiseLists;
+private List<User> users;
+
 
     public Administrator(String roleId) {
         super(roleId);
+        this.users = new ArrayList<>();
     }
 
     public Administrator(List<Advertise> advertiseLists, String roleId) {
         super(roleId);
         this.advertiseLists = advertiseLists;
+        this.users = new ArrayList<>();
     }
 
     public List<Advertise> getAdvertiseLists() {
         return advertiseLists;
+    }
+
+    public void addUsers(List<User> listOfUser) {
+        this.users.addAll(listOfUser);
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    @Override
+    public String toString() {
+        return "Administrator{" +
+                "advertiseLists=" + advertiseLists +
+                ", users=" + users +
+                '}';
     }
 }
